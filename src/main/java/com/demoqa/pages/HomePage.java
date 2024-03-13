1c27822cc6f2b138c2d8971acbcef8981a507212
aliyasagut <aliya.sagut@gmail.com>
2024-03-13 13:24:59 +0100

    update

---
+++ src/main/java/com/demoqa/pages/HomePage.java
@@ -0,0 +1,28 @@
+package com.demoqa.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends BasePage{
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(css = ".card:nth-child(2)")
+    WebElement forms;
+
+    public SidePanel getForms() {
+        click(forms);
+        return new SidePanel(driver);
+    }
+
+    @FindBy(css = ".fc-button-label")
+    WebElement buttonLabel;
+
+    public HomePage confirmCookie() {
+        click(buttonLabel);
+        return this;
+    }
+}
---
+++ src/test/java/com/demoqa/tests/PracticeFormTests.java
@@ -0,0 +1,29 @@
+package com.demoqa.tests;
+
+import com.demoqa.pages.HomePage;
+import com.demoqa.pages.PracticeFormPage;
+import com.demoqa.pages.SidePanel;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class PracticeFormTests extends TestBase {
+
+    @BeforeMethod
+    public void precondition() {
+        new HomePage(driver)
+//                .confirmCookie()
+                .getForms();
+        new SidePanel(driver).selectPracticeForm();
+    }
+
+    @Test
+    public void createStudentAccountPositiveTest() {
+//        new PracticeFormPage(driver).hideIframes();
+        new PracticeFormPage(driver).enterPersonalData("Jack", "Sparrow", "jack@jack.com", "123456789")
+                .selectGender("Male")
+                .typeOfDate("16 August 1987")
+                .addSubjects(new String[]{"Maths", "Chemistry", "English"})
+                .selectHobby(new String[]{"Sport", "Music"})
+                .uploadFile("C:/QA/logo.jpg");
+    }
+}
